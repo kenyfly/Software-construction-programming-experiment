fefea95ef04eb12cacb5d4d6228ca07c9c246e2d
kenyfly <55352554+kenyfly@users.noreply.github.com>
2019-09-16 19:12:23 +0800

    需求更新和代码优化

--- p1/p1.java
+++ p1/p1.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 class p1{
     
-    String[] jie = {" ", "(", ")", "{", "}", "\0"};
-    String[] yun = {"+", "-", "*", "/", "&", "|", "^", "%"};
-    String[] zifuchuan = {"\"", "\'"};
-    String[] guanjianzi = {"if", "else", "for", "while"};
+    private static String[] jie = {" ", "(", ")", "{", "}", "\0"};
+    private static String[] yun = {"+", "-", "*", "/", "&", "|", "^", "%", "="};
+    private static String[] zifuchuan = {"\"", "\'"};
+    private static String[] guanjianzi = {"if", "else", "for", "while"};
 
-    public boolean isNum(String S){
+    public static boolean isNum(String S){
         for(int i = 0; i < S.length(); i++){
-            if(!(S.charAt(i) >= '0' && S.charAt(i) <= '9')){
+            if(!(S.charAt(i) >= '0' && S.charAt(i) <= '9' || S.charAt(i) == '.')){
                 return false;
             }
         }
         return true;
     }
 
-    public String[] cutString(String S, int cur){
+    public static String[] cutString(String S, int cur){
         
         String[] sArr = {"", ""};
         for(int i = 0; i < S.length(); i++){
@@ -29,7 +29,7 @@ class p1{
         return sArr;
     }
 
-    public boolean cmpString(String[] arr, String S){
+    public static boolean cmpString(String[] arr, String S){
         for(int i = 0; i < arr.length; i++){
             if(arr[i].equals(S)){
                 return true;
@@ -38,35 +38,35 @@ class p1{
         return false;
     }
 
-    public void cmp(String S){
+    public static void cmp(String S){
         for(int i = 0, isString = 0; i < S.length(); i++){
             String a = new String()+S.charAt(i);
-            if(new p1().cmpString(jie, a) || new p1().cmpString(yun, a) || new p1().cmpString(zifuchuan, a)){
+            if(p1.cmpString(jie, a) || p1.cmpString(yun, a) || p1.cmpString(zifuchuan, a)){
                 if(i == 0){
                     
-                    if(new p1().cmpString(zifuchuan, a)){
+                    if(p1.cmpString(zifuchuan, a)){
                         if(isString == 0){
                             isString = 2;
                         }
                         isString--;
                         System.out.printf("(%c, 字符串标记符)\n", S.charAt(i));
-                    } else if(new p1().cmpString(jie, a)){
+                    } else if(p1.cmpString(jie, a)){
                         System.out.printf("(%c, 界符)\n", S.charAt(i));
                     } else{
                         System.out.printf("(%c, 运算符)\n", S.charAt(i));
                     }
-                    S = new p1().cutString(S, 1)[1];
+                    S = p1.cutString(S, 1)[1];
                 } else{
                     String[] req;
                     String ans;
-                    req = new p1().cutString(S, i);
+                    req = p1.cutString(S, i);
                     ans = req[0];
                     S = req[1];
                     if(isString == 1){
                         System.out.printf("(%s, 字符串)\n", ans);
-                    } else if(new p1().isNum(ans)){
+                    } else if(p1.isNum(ans)){
                         System.out.printf("(%s, 数字常量)\n", ans);
-                    } else if(new p1().cmpString(guanjianzi, ans)){
+                    } else if(p1.cmpString(guanjianzi, ans)){
                         System.out.printf("(%s, 关键字)\n", ans);
                     } else{
                         System.out.printf("(%s, 标识符)\n", ans);
@@ -83,7 +83,7 @@ class p1{
             String S = in.nextLine();
             S += "\0";
             System.out.printf("%s %d\n", S, S.length());
-            new p1().cmp(S);
+            p1.cmp(S);
         }
         in.close();
     }
